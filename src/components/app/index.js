6d9419c79c3776e13b0e1cb3b5fac11b6bc051b7
Mama <mamatouresouare@MacBook-Pro-de-Mama.local>
2022-02-28 20:17:43 +0100

    fix le projet 2

---
+++ src/components/app/index.js
@@ -0,0 +1,15 @@
+import Navigation from '../navigation';
+import Search from '../search';
+
+function App() {
+  const blabla = "sunne"
+
+  return (
+    <div className="App">
+      <Navigation />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search/index.js
@@ -0,0 +1,23 @@
+function search() {
+  const blabla = "sunne"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>{blabla}</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default search;
